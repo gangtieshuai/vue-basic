7028288cd29548ac633a269f538642685328fae4
郭德纲 <40748564+gangtieshuai@users.noreply.github.com>
2020-11-26 10:57:47 +0800

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+/*
+ * @Author: 郭德纲
+ * @Date: 2020-11-11 09:23:57
+ * @LastEditors: 郭德纲
+ * @LastEditTime: 2020-11-16 16:35:51
+ * @Description: file content
+ */
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': 0,
+    'space-before-function-paren': 0,
+    'indent': ['off', 2]
+  },
+}
--- README.md
+++ README.md
@@ -1,12 +1,30 @@
-# vue-basic
-The basic background management system framework built by Vue
+# ccb-web
 
+> A Vue.js project
 
-# npm install
+## Build Setup
 
-# npm run dev 
-defalut port 8080
+``` bash
+# install dependencies
+npm install
 
-# npm run build
+# serve with hot reload at localhost:8080
+npm run dev
 
+# build for production with minification
+npm run build
 
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
